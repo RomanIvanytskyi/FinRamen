7382dd34474526e771483837109731f65b82a31d
RomanIvanytskyi <roman.ivanytskyi21@gmail.com>
2022-09-29 16:46:31 +0300

    FIN-18 category routes

--- server/routes/categoryRouter.js
+++ server/routes/categoryRouter.js
@@ -1,12 +1,11 @@
 const Router = require("express");
 const router = new Router();
 const controller = require("../controllers/categoryController");
+const authMiddleware = require("../middleware/authMiddleware");
 
-
-
-router.post("/create", controller.create);
-router.put("/update", controller.update);
-router.post("/delete", controller.delete);
+router.post("/create", authMiddleware, controller.create);
+router.put("/update", authMiddleware, controller.update);
+router.post("/delete", authMiddleware, controller.delete);
 router.post("/get", controller.get);
 
 module.exports = router;
