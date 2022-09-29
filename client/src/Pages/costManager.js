231c3112770a89df7cda8ef5a8078a691c3d5175
RomanIvanytskyi <roman.ivanytskyi21@gmail.com>
2022-09-29 04:13:50 +0300

    FIN-12 Pages components examples

--- client/src/Pages/costManager.js
+++ client/src/Pages/costManager.js
@@ -1,9 +1,34 @@
-import React from "react";
+import { React } from "react";
 import { connect } from "react-redux";
 import { Navigate } from "react-router-dom";
+import "./pages.css";
+import "./costs.css";
+import AddCosts from "../Components/AddCosts";
+import Transactions from "../Components/Transactions";
 const Costs = (props) => {
   return (
-    <div>{props.auth?.user ? <div>Costs</div> : <Navigate to={"/"} />}</div>
+    <div className="main">
+      {props.auth?.user ? (
+        <div className="Content">
+          <div class="container">
+            <div class="head">
+              <div class="Balance">Balance</div>
+            </div>
+
+            <div class="Transactions">
+              <Transactions />
+            </div>
+            <div class="Create-Transaction">
+              {" "}
+              Add new transaction
+              <AddCosts className="addCosts" />
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Navigate to={"/"} />
+      )}
+    </div>
   );
 };
 const mapStateToProps = (state, ownProps) => ({
